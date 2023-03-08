008ea65316b4fcaefc41d193fb90dbd6355afdb1
JojoGuedel <67550456+JojoGuedel@users.noreply.github.com>
2023-03-08 18:03:41 +0100

    hello triangle

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define GLFW_INCLUDE_NONE
-
-#include "GLFW/glfw3.h"
 #include "glad/gl.h"
+#include "GLFW/glfw3.h"
 
 typedef const char *string;
 
@@ -24,32 +24,32 @@ unsigned int shader_program;
 unsigned int vbo;
 unsigned int vao;
 
-char* load_shader_src(const char* path) {
-    char* buffer;
+char* load_shader_src(const char* path, size_t size) {
+    char* buffer = calloc(size, 1);
     FILE* file;
-    long size;
 
-    if(fopen_s(&file, path, "rb"))
+    if(fopen_s(&file, path, "rb")) {
         printf("[Error]: Could not open '%s'.\n", path);
+        return NULL;
+    }
 
-    fseek(file, 1, SEEK_END);
-    size = ftell(file);
-    buffer = malloc(size);
     fread(buffer, 1, size, file);
     fclose(file);
+
+    printf("[Info] - loading shader: \n%s\n", buffer);
 
     return buffer;
 }
 
 void key_cb(GLFWwindow *window, int key, int scancode, int action, int mods) {
-    printf("[Info]: keypress (%c,%i,%i,%i)\n", key, scancode, action, mods);
+    printf("[Info] - keypress: (%c,%i,%i,%i)\n", key, scancode, action, mods);
 }
 
 void glfw_error_cb(int error_code, const char* description) {
-    printf("[Error]: (glfw%i) %s", error_code, description);
+    printf("[Error] - glfw(%i): %s", error_code, description);
 }
 
-void GLAPIENTRY gl_error_cb(
+void GLAPIENTRY gl_debug_cb(
     GLenum source, 
     GLenum type, 
     GLuint id, 
@@ -59,46 +59,64 @@ void GLAPIENTRY gl_error_cb(
     const void *userParam
     ) {
     printf(
-        "[Error]: (opengl) %s type = 0x%x, severity = 0x%x, message = %s\n",
-        (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, severity,
+        "[%s] - opengl: type = 0x%x, severity = 0x%x, message = %s\n", (type == GL_DEBUG_TYPE_ERROR? "Error" : "Info"),
+        type, 
+        severity,
         message
     );
 }
 
+void check_error(GLuint shader, GLenum name) {
+    int success;
+    char infoLog[512];
+
+    glGetShaderiv(shader, name, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        printf("[Error] - opengl shader: %s\n", infoLog);
+    }
+}
+
 void init() {
     /* setup vertex shader */
-    const char* vertex_shader_src = load_shader_src("main.vert");
+    const char* vertex_shader_src = load_shader_src("./src/shader/main.vert", 200);
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_src, NULL);
     glCompileShader(vertex_shader);
-
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-
-    if (!success) {
-        glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
-        printf("[Error]: vertexshader could not be compiled.\n%s\n", infoLog);
-    }
+    free((char*)vertex_shader_src);
+    check_error(vertex_shader, GL_COMPILE_STATUS);
 
     /* setup fragment shader */
-
-    // TODO: continue...
+    const char* fragment_shader_src = load_shader_src("./src/shader/main.frag", 200);
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_src, NULL);
+    glCompileShader(fragment_shader);
+    free((char*) fragment_shader_src);
+    check_error(fragment_shader, GL_COMPILE_STATUS);
     
-    free((char*)vertex_shader_src);
+    /* setup shader program */
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
 
+    /* setup vertex array */
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
     /* setup vertex buffer*/
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, sizeof(vertices) / sizeof(float) / 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
-
-    /* setup vertex array */
-    glGenVertexArrays(1, &vao);
 }
 
 void  render() {
+    // glUseProgram(shader_program);
+
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -120,7 +138,6 @@ int main(int argc, string args[]) {
         return -1;
     }
 
-
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -132,7 +149,9 @@ int main(int argc, string args[]) {
     gladLoadGL(glfwGetProcAddress);
 
     glEnable(GL_DEBUG_OUTPUT);
-    glDebugMessageCallback(gl_error_cb, 0);
+    glDebugMessageCallback(gl_debug_cb, 0);
+
+    init();
 
     while (!glfwWindowShouldClose(window)) {
         render();
